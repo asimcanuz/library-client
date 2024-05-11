cead2fd7062a464aa3019db9fe26f0c392df13b3
asimcanuz <asimcanuz@gmail.com>
2024-05-12 00:41:23 +0300

    add redux

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import App from "./App.tsx";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthProvider>
+    <Provider store ={store}>
+      <AuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
+    </Provider>
   </React.StrictMode>
 );
