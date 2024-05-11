60011b7573e940f87ab8f4b20561be73b492b047
asimcanuz <asimcanuz@gmail.com>
2024-05-12 00:34:59 +0300

    use BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>
 );
