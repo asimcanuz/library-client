f9a3b54cd49e377296aab9ae7c76357225f35992
asimcanuz <asimcanuz@gmail.com>
2024-05-12 00:33:48 +0300

    update authContext

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,13 +1,60 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, Dispatch, SetStateAction, useState } from "react";
 
-export const AuthContext = createContext({});
 
-export const AuthProvider = ({ children }: React.PropsWithChildren) => {
-  const [auth, setAuth] = useState({});
-  const [persist, setPersist] = useState<boolean>(false);
+type AuthState = {
+  username?: string;
+  accessToken?: string;
+  role?: string[];
+}
+
+
+type AuthContextActions = {
+  setAuth: Dispatch<SetStateAction<AuthState>>;
+  setPersist: Dispatch<SetStateAction<boolean>>;
+}
+
+
+type AuthContextType = {
+  auth: AuthState;
+  persist: boolean;
+  setAuth: (auth: AuthState) => void;
+  setPersist: (persist: boolean) => void;
+}
+
+
+export const AuthContext = createContext<AuthContextType>({
+  auth: { username: "", accessToken: "", role: [] },
+  persist: false,
+  setAuth: () => {},
+  setPersist: () => {},
+});
+
+type AuthProviderProps = {
+  children: React.ReactNode;
+}
+
+
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const [auth, setAuth] = useState<AuthState>(() => {
+    const storedAuth = localStorage.getItem("auth");
+    if (storedAuth) {
+      return JSON.parse(storedAuth);
+    } else {
+      return { username: "", accessToken: "",  roles: [] };
+    }
+  });
+  
+  const [persist, setPersist] = useState<boolean>(() => {
+    const storedPersist = localStorage.getItem("persist");
+    return storedPersist ? JSON.parse(storedPersist) : false;
+  });
+
+
+
+  const value:AuthContextType = {auth, persist, setAuth, setPersist}; 
 
   return (
-    <AuthContext.Provider value={{ auth, persist, setAuth, setPersist }}>
+    <AuthContext.Provider value={value}>
       {children}
     </AuthContext.Provider>
   );
